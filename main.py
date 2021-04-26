ebccc87ce882c75188d268f6698f41fb14141da3
leila571 <leilamarks@postoakschool.org>
2021-04-26 10:10:32 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+#List of things I need to incorporate
+#GUI
+#MP3 file importer
+#CSV file
+#Something that keeps track of score
+
+#MP3 File Player
+from pygame import mixer  # Load the popular external library
+
+#MP3 file importer
+mixer.init()
+mixer.music.load(C:\Users\17138\Desktop\Music\O1 Black Cow.m4a)
+mixer.music.play()
